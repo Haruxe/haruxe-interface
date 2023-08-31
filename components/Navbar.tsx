34e740b5a8720fee015fa5431549bba9a04b66cb
Haruxe <haruxe@proton.me>
2023-08-30 19:35:10 -0700

    more

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,28 +8,28 @@ function Navbar() {
   return (
     <div className="mx-auto py-3 flex place-items-center">
       <div>
-        <Link href="/" className="duration-150 hover:text-red-400">
-          <h1 className="text-4xl font-black text-center">HARUXE</h1>
+        <Link href="/" className="duration-150 hover:text-red-500">
+          <h1 className="text-3xl font-black text-center">HARUXE</h1>
         </Link>
       </div>
       <div className="ml-auto flex space-x-3 place-items-center place-content-center">
         <Link
           href="https://linkedin.com/in/joshfrancisco"
-          className="duration-150 hover:text-red-400"
+          className="duration-150 hover:text-red-500"
         >
-          <Linkedin className="w-4 h-4" />
+          <Linkedin className="w-5 h-5" />
         </Link>
         <Link
           href="https://github.com/Haruxe"
-          className="duration-150 hover:text-red-400"
+          className="duration-150 hover:text-red-500"
         >
-          <Github className="w-4 h-4" />
+          <Github className="w-5 h-5" />
         </Link>
         <Link
           href="https://github.com/Haruxe"
-          className="duration-150 hover:text-red-400"
+          className="duration-150 hover:text-red-500"
         >
-          <Twitter className="w-4 h-4" />
+          <Twitter className="w-5 h-5" />
         </Link>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,31 +9,61 @@ const Home: NextPage = () => {
   return (
     <div className="space-y-12">
       <Head>
-        <title>Haruxe | Home</title>
+        <title>HARUXE</title>
         <meta name="description" content="Home of Haruxe's Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="mt-12 space-y-3 w-1/2">
-        <h1 className="text-5xl font-bold">Josh Francisco</h1>
-        <p className="text-lg font-bold opacity-80">
-          Web3 Developer and Security Researcher
-        </p>
-        <p className="text-zinc-400">
-          I build things on Ethereum. I&apos;m currently working on{" "}
-          <Link
-            href="https://baofinance.io"
-            className="text-white font-bold duration-150 hover:text-red-400"
-          >
-            Bao Finance
-          </Link>
-          {""} and{" "}
-          <Link
-            href="https://baofinance.io"
-            className="text-white font-bold duration-150 hover:text-red-400"
-          >
-            Blueberry Foundation
-          </Link>
-        </p>
+      <div className="space-x-5 flex place-content-between">
+        <div className="mt-12 space-y-3 w-[40%]">
+          <h1 className="text-5xl font-bold">Josh Francisco</h1>
+          <p className="text-lg text-zinc-200">
+            Web3 Developer and Security Researcher
+          </p>
+          <p className="text-zinc-400">
+            I build things on Ethereum, with an emphasis on security and
+            reliability.
+          </p>
+        </div>
+        <div className="mt-12 space-y-3 w-[45%]">
+          <p className="text-zinc-400 leading-relaxed">
+            Back in 2019, I started my journey in programming. I started with
+            game development, making{" "}
+            <Link
+              href="https://harux.itch.io/"
+              className="font-bold text-white duration-150 hover:text-red-500"
+            >
+              independent games
+            </Link>{" "}
+            with Unity and C#. Eventually I was enthralled by the world of
+            blockchain technology and the potential it has to change the world.
+            <br /> <br />
+            Since then, I&apos;ve built tooling for NFT projects like{" "}
+            <Link
+              href="https://kaijukingz.io"
+              className="font-bold text-white duration-150 hover:text-red-500"
+            >
+              Kaiju Kingz
+            </Link>
+            , and had the opportunity to work with some amazing minds developing
+            and designing software for{" "}
+            <Link
+              href="https://baofinance.io"
+              className="font-bold text-white duration-150 hover:text-red-500"
+            >
+              BAO Finance
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="https://bloom.garden"
+              className="font-bold text-white duration-150 hover:text-red-500"
+            >
+              Blueberry Foundation
+            </Link>
+            .<br /> <br />
+            When I&apos;m not building, I&apos;m usually playing video games, at
+            the gym, or hanging out with my dog.
+          </p>
+        </div>
       </div>
     </div>
   );
